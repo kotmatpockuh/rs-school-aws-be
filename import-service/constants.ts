6471636f656112d02ab8320aefbeb453f921f59d
Pavel Dobrenko <dobrenko92@gmail.com>
2020-11-13 17:28:54 +0300

    feat: task-5 add signedUrl

---
+++ import-service/constants.ts
@@ -0,0 +1,1 @@
+export const BUCKET = 'pav-import-data';
--- import-service/handlers/import-products.ts
+++ import-service/handlers/import-products.ts
@@ -8,6 +8,10 @@ import {
     formattedErrorResponse,
     formattedSuccessResponse,
 } from '../../shared/helpers/response.helper';
+import AWS from 'aws-sdk';
+import { BUCKET } from '../constants';
+import { throwError } from '../../shared/helpers/error.helper';
+import { ErrorsEnum } from '../../shared/types/errors.enum';
 
 export const importProductsFile: APIGatewayProxyHandler = async (
     event: APIGatewayProxyEventBase<APIGatewayEventDefaultAuthorizerContext>
@@ -15,7 +19,21 @@ export const importProductsFile: APIGatewayProxyHandler = async (
     console.log('📝 importProductsFile: ', event);
 
     try {
-        return formattedSuccessResponse({});
+        const fileName = event?.queryStringParameters?.name;
+
+        if (!fileName) {
+            throwError(ErrorsEnum.WrongRequest, 400);
+        }
+
+        const s3 = new AWS.S3({ region: 'eu-west-1' });
+
+        const url = s3.getSignedUrl('getObject', {
+            Bucket: BUCKET,
+            Key: `uploaded/${fileName}`,
+            Expires: 60,
+        });
+
+        return formattedSuccessResponse(url);
     } catch (error) {
         return formattedErrorResponse(error);
     }
