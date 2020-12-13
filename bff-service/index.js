eb4b6fd69ffe24f0b2bb78bdd32b497cfb0169b2
Pavel Dobrenko <dobrenko92@gmail.com>
2020-12-13 15:53:33 +0300

    feat: task-9 add bff service

---
+++ bff-service/index.js
@@ -0,0 +1,65 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios').default;
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const cacheObj = {};
+
+app.use(express.json());
+
+app.all('/*', (req, res) => {
+    const { originalUrl, method, body } = req;
+
+    const recipient = originalUrl.split('/')[1];
+    const recipientUrl = process.env[recipient];
+    const requestUrl = `${recipientUrl}${originalUrl}`;
+
+    if (recipientUrl) {
+        const isCachedUrl =
+            method === 'GET' && originalUrl.endsWith(process.env.cached_url);
+
+        const config = {
+            method,
+            url: requestUrl,
+            ...(Object.keys(req.body || {}).length > 0 && { data: body }),
+        };
+
+        if (
+            isCachedUrl &&
+            cacheObj[requestUrl] &&
+            cacheObj[requestUrl].expires > Date.now()
+        ) {
+            res.json(cacheObj[requestUrl].value);
+            return;
+        }
+
+        axios(config)
+            .then((resp) => {
+                if (isCachedUrl) {
+                    cacheObj[requestUrl] = {
+                        value: resp.data,
+                        expires: Date.now() + 2 * 60 * 1000 // wait for 2 minutes,
+                    };
+                }
+
+                res.json(resp.data);
+            })
+            .catch((err) => {
+                console.error('⚠️ Error', err);
+
+                if (err.response) {
+                    const { status, data } = err.response;
+
+                    res.status(status).json(data);
+                } else {
+                    res.status(500).json({ error: err.message });
+                }
+            });
+    } else {
+        res.status(502).json({ error: '⚠️ Cannot process the request' });
+    }
+});
+
+app.listen(PORT);
